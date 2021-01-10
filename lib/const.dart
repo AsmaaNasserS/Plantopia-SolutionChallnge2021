821f00a260500a7d22c7d19b3591647a89a3a05b
MayarELawady <mayarhanyelawady@gmail.com>
2021-01-10 23:40:39 +0200

    uploaded

---
+++ lib/const.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+const kInActivelogInButtonColor = Color(0xFF00B761); // login + get started + sign up + any green button
+const kActivelogInButtonColor = Color(0xFF0B9755); // login + get started + sign up + any green button
+
+const kInActiveBackButtonColor = Color(0xFFEBEBEB); // or any grey button
+const kActiveBackButtonColor = Color(0xFFD5D5D5);
+
+const kBottomFootbarContainerColour = Color(0xFFEBEBEB);
+
+const klogInButtonTextStyle = TextStyle(
+  fontSize:18.00,
+  fontWeight: FontWeight.bold,
+  color : Color(0xFFF8F8F8), // text of button color
+);
+
+const kWelcomeScreensTitleText = TextStyle(
+  fontSize: 25.00,
+  fontWeight: FontWeight.bold,
+  color: Colors.black,
+);
+
+const kWelcomeScreensDescriptionText = TextStyle(
+  fontSize: 15.00,
+  fontWeight: FontWeight.bold,
+  color: Colors.black,
+);
+
+
+
+//////////////////// Summary of Colors///////////////
+//  InActiveGreen == > Color(0xFF00B761)
+//  ActiveGreen == >Color(0xFF0B9755)
+// InActiveOrange == > Color(0xFFFE19538)
+// LightGrey ==> Color(0xFFEBEBEB);
+// Grey ==> Color(0xFFD5D5D5);
+// Titles font size ==> 25
+// descriptions font sizes ==> 15
